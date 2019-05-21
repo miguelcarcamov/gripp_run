bee6bbac14eae240295b6535951ac05c8de04cf3
Miguel Carcamo <miguel.carcamo@protonmail.com>
2019-05-21 11:17:19 +0100

    first

---
+++ RMSynthesis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+echo "========="
+ls -la
+echo "=================="
+echo "Print environment details"
+printenv
+echo "========="
+echo $PWD
+singularity --version
+
+#mkdir prmon && tar xf prmon_1.0.1_x86_64-static-gnu72-opt.tar.gz -C prmon --strip-components 1
+tar -xzcf HPC_data.tar.gz
+#./prmon/bin/prmon -p $$ -i 10 &
+
+cd meerkat
+
+singularity -d  exec  --cleanenv --home $PWD:/srv --pwd /srv -C  shub://miguelcarcamov/container_docker:hpc './run.sh'
+#mv prmon.txt prmon$1.txt
+ls -ltrh
