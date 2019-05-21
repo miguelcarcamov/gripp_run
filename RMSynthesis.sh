2c5f98ece2ecc26cc273307481db76f6d88c9e3a
Miguel Carcamo <miguel.carcamo@protonmail.com>
2019-05-21 14:57:54 +0100

    fix tar

--- RMSynthesis.sh
+++ RMSynthesis.sh
@@ -9,7 +9,7 @@ echo "========="
 singularity --version
 
 mkdir prmon && tar xf prmon_1.0.1_x86_64-static-gnu72-opt.tar.gz -C prmon --strip-components 1
-tar -xzcf HPC_data.tar.gz
+tar -xzvf HPC_data.tar.gz
 ./prmon/bin/prmon -p $$ -i 10 &
 
 cd meerkat
