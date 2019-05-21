045e565f68ef5f2af2c2d9f14c82b98970715b49
miguelcarcamov <miguel.carcamo@protonmail.com>
2019-05-21 12:06:12 +0100

    trying fix

--- RMSynthesis.sh
+++ RMSynthesis.sh
@@ -9,12 +9,12 @@ echo "========="
 echo $PWD
 singularity --version
 
-#mkdir prmon && tar xf prmon_1.0.1_x86_64-static-gnu72-opt.tar.gz -C prmon --strip-components 1
+mkdir prmon && tar xf prmon_1.0.1_x86_64-static-gnu72-opt.tar.gz -C prmon --strip-components 1
 tar -xzcf HPC_data.tar.gz
-#./prmon/bin/prmon -p $$ -i 10 &
+./prmon/bin/prmon -p $$ -i 10 &
 
 cd meerkat
 
 singularity -d  exec  --cleanenv --home $PWD:/srv --pwd /srv -C  shub://miguelcarcamov/container_docker:hpc './run.sh'
-#mv prmon.txt prmon$1.txt
+mv prmon.txt prmon.txt
 ls -ltrh
