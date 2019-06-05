fc4f8da4774a88fc2e9cbbb5f0da6a0b41d91014
miguelcarcamov <miguel.carcamo@protonmail.com>
2019-06-05 14:42:35 +0100

    merge

--- run.sh
+++ run.sh
@@ -10,8 +10,8 @@ ls -lah
 echo "Let's see how many processors our machine has"
 
 lscpu | grep -E '^Thread|^Core|^Socket|^CPU\('
-#PROCS=$(lscpu | grep -E '^Thread|^Core|^Socket|^CPU\(' | grep "CPU" | grep -o '[[:digit:]]*')
-PROCS=8
+PROCS=$(lscpu | grep -E '^Thread|^Core|^Socket|^CPU\(' | grep "CPU" | grep -o '[[:digit:]]*')
+#PROCS=8
 
 echo "Running RMSynthesis with $PROCS cores"
 echo "Running command: python3 /rm_synthesis_simple/realdata_test_parallel.py meerkat/freqlist.txt meerkat/data.par meerkat/sim_Q.fits meerkat/sim_U.fits meerkat/sim_Q.fits meerkat/output_test $PROCS 1000 True 0.05 > outputtxt.txt"
