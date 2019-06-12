4b9711b0f01b288f39d43eb40d7a1f88e9b06e66
miguelcarcamov <miguel.carcamo@protonmail.com>
2019-06-12 14:17:31 +0100

    chaanges

--- RMSynthesis2.sh
+++ RMSynthesis2.sh
@@ -43,7 +43,7 @@ ls -lah
 
 echo "Ok! Let's run the container"
 echo "run2.sh file is going to run with the following parameters: $1 $2 $3 $4 $result_string"
-singularity exec --cleanenv -H $PWD:/srv -B $PWD:/srv -W $PWD:/srv/tmp -C shub://miguelcarcamov/container_docker:hpc bash run2.sh $1 $2 $3 $4 $result_string
+singularity exec --cleanenv -H $PWD:/srv -B $PWD:/srv -W $PWD:/srv -C shub://miguelcarcamov/container_docker:hpc bash run2.sh $1 $2 $3 $4 $result_string
 
 mv prmon.txt prmon$2_$3.txt
 ls -ltrh
