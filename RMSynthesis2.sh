b48d8a3d6f51d52b9d07509aa75d9fe58ff9ae81
Miguel Carcamo <miguel.carcamo@protonmail.com>
2019-06-12 14:36:24 +0100

    Fix

--- RMSynthesis2.sh
+++ RMSynthesis2.sh
@@ -16,7 +16,7 @@ echo $3 #id_end
 echo $4 #experiment
 
 echo "Extracting Process Monitor - This is to monitor the processes that we will run"
-mkdir prmon && tar xf prmon_1.0.1_x86_64-static-gnu72-opt.tar.gz -C prmon --strip-components 1
+mkdir -p prmon && tar xf prmon_1.0.1_x86_64-static-gnu72-opt.tar.gz -C prmon --strip-components 1
 echo "Extracting our dataset. This contains the Meerkat Simulated data with 10000 sources in a 2048x2048 image"
 cat HPC_data.tar.gz.* | tar xzvf -
 echo "Running prmon"
@@ -32,7 +32,7 @@ echo "Let's see how many processors our machine has"
 lscpu | grep -E '^Thread|^Core|^Socket|^CPU\('
 
 echo "Lets create a tmp folder to keep temporal files"
-mkdir tmp
+mkdir -p tmp
 
 echo "Lets create a folder to save the results"
 result_string=results_experiment_$4/
@@ -43,7 +43,7 @@ ls -lah
 
 echo "Ok! Let's run the container"
 echo "run2.sh file is going to run with the following parameters: $1 $2 $3 $4 $result_string"
-singularity exec --cleanenv -H $PWD:/srv -B /srv -W /srv/tmp -C shub://miguelcarcamov/container_docker:hpc bash run2.sh $1 $2 $3 $4 $result_string
+singularity exec --cleanenv -H $PWD:/srv --pwd /srv -C shub://miguelcarcamov/container_docker:hpc bash run2.sh $1 $2 $3 $4 $result_string
 
 mv prmon.txt prmon$2_$3.txt
 ls -ltrh
