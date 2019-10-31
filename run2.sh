95ceeaabd6334c571683a7d1661a7dd0f6342e18
miguelcarcamov <miguel.carcamo@protonmail.com>
2019-10-31 13:39:04 +0000

    my

--- run2.sh
+++ run2.sh
@@ -24,6 +24,6 @@ lscpu | grep -E '^Thread|^Core|^Socket|^CPU\('
 real_end=$(($3-1))
 
 echo "Running RMSynthesis with $1 cores"
-echo "Running command: python3 rm_synthesis_simple/realdata_parallel_los.py meerkat/$4/freqlist.txt $2 $3 meerkat/$4/sim_Q.fits meerkat/$4/sim_U.fits meerkat/$4/sim_Q.fits $5 $1 True 1e-10 1e-6 Thin False > outputtxt_$2_$real_end.txt"
+echo "Running command: python3 rm_synthesis_simple/realdata_parallel_los.py meerkat/$4/freqlist.txt $2 $3 meerkat/$4/sim_Q.fits meerkat/$4/sim_U.fits meerkat/$4/sim_Q.fits $5 $1 True 1e-8 1e-4 Thin False > outputtxt_$2_$real_end.txt"
 
-python3 rm_synthesis_simple/realdata_parallel_los.py meerkat/$4/freqlist.txt $2 $3 meerkat/$4/sim_Q.fits meerkat/$4/sim_U.fits meerkat/$4/sim_Q.fits $5 $1 True 1e-10 1e-6 Thin False > outputtxt_$2_$real_end.txt
+python3 rm_synthesis_simple/realdata_parallel_los.py meerkat/$4/freqlist.txt $2 $3 meerkat/$4/sim_Q.fits meerkat/$4/sim_U.fits meerkat/$4/sim_Q.fits $5 $1 True 1e-8 1e-4 Thin False > outputtxt_$2_$real_end.txt
